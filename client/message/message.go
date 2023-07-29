43ed010c566827851a54c534545c2cffc854f14e
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-07-28 22:59:07 -0400

    msg nonce

--- client/message/message.go
+++ client/message/message.go
@@ -5,6 +5,9 @@ MESSAGE FORMAT
 | message type
 -----------------
 | 4 bytes
+| message nonce
+-----------------
+| 4 bytes
 | message len
 -----------------
 | len bytes
@@ -16,8 +19,10 @@ package message
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/binary"
 	"go-dmtor/logger"
+	mrand "math/rand"
 )
 
 var log = logger.New()
@@ -35,25 +40,46 @@ const (
 )
 
 type Message struct {
-	Type MsgType
-	Len  uint32
-	Body []byte
+	Type  MsgType
+	Nonce uint32
+	Len   uint32
+	Body  []byte
 }
 
 func NewMessageText(msg string) *Message {
 	return &Message{
-		Type: MSG,
-		Len:  uint32(len(msg)),
-		Body: []byte(msg),
+		Type:  MSG,
+		Nonce: nonce(),
+		Len:   uint32(len(msg)),
+		Body:  []byte(msg),
+	}
+}
+
+func NewMessageAck() *Message {
+	return &Message{
+		Type:  ACK,
+		Nonce: nonce(),
+		Len:   0,
+		Body:  nil,
 	}
 }
 
 func NewMessageHello() *Message {
 	return &Message{
-		Type: HELLO,
-		Len:  0,
-		Body: nil,
+		Type:  HELLO,
+		Nonce: nonce(),
+		Len:   0,
+		Body:  nil,
 	}
+}
+
+func nonce() uint32 {
+	b := make([]byte, 4)
+	_, err := rand.Read(b)
+	if err != nil {
+		return mrand.Uint32()
+	}
+	return binary.BigEndian.Uint32(b)
 }
 
 func (t MsgType) String() string {
@@ -80,14 +106,22 @@ func (t MsgType) String() string {
 func (m *Message) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 
+	// msg type
 	if err := binary.Write(buf, binary.BigEndian, m.Type); err != nil {
 		return nil, err
 	}
 
+	// msg nonce
+	if err := binary.Write(buf, binary.BigEndian, m.Nonce); err != nil {
+		return nil, err
+	}
+
+	// msg len
 	if err := binary.Write(buf, binary.BigEndian, m.Len); err != nil {
 		return nil, err
 	}
 
+	// msg body
 	if err := binary.Write(buf, binary.BigEndian, m.Body); err != nil {
 		return nil, err
 	}
@@ -100,6 +134,7 @@ func Deserialize(data []byte) (*Message, error) {
 	// log message bytes
 	log.Debugf("message bytes: %v\n", data)
 
+	// type
 	var msgType uint32
 	if err := binary.Read(buf, binary.BigEndian, &msgType); err != nil {
 		log.Errorf("error reading message type: %v\n", err)
@@ -107,6 +142,14 @@ func Deserialize(data []byte) (*Message, error) {
 	}
 	log.Debugf("Deserialized message type: %v\n", msgType)
 
+	// msg nonce
+	var msgNonce uint32
+	if err := binary.Read(buf, binary.BigEndian, &msgNonce); err != nil {
+		log.Errorf("error reading message nonce: %v\n", err)
+		return nil, err
+	}
+
+	// msg len
 	var msgLen uint32
 	if err := binary.Read(buf, binary.BigEndian, &msgLen); err != nil {
 		log.Errorf("error reading message len: %v\n", err)
@@ -114,15 +157,13 @@ func Deserialize(data []byte) (*Message, error) {
 	}
 	log.Debugf("Deserialized message len: %v\n", msgLen)
 
-	body := make([]byte, 0)
-	if msgLen > 0 {
-		body = make([]byte, msgLen)
-		if err := binary.Read(buf, binary.BigEndian, &body); err != nil {
-			log.Errorf("error reading message body: %v\n", err)
-			return nil, err
-		}
+	// msg body
+	body := make([]byte, msgLen)
+	if err := binary.Read(buf, binary.BigEndian, &body); err != nil {
+		log.Errorf("error reading message body: %v\n", err)
+		return nil, err
 	}
 	log.Debugf("Deserialized message body: %v\n", body)
 
-	return &Message{MsgType(msgType), msgLen, body}, nil
+	return &Message{MsgType(msgType), msgNonce, msgLen, body}, nil
 }
