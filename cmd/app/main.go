4be94c4bf692f8c569432430891a61fe93854480
kasparov <32327548+1F47E@users.noreply.github.com>
2023-11-21 11:07:49 -0300

    chat window

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"github.com/1F47E/go-shaihulud/client"
 	myrsa "github.com/1F47E/go-shaihulud/cryptotools/rsa"
@@ -33,11 +32,11 @@ func main() {
 	go t.RenderChat()
 	// eventsCh <- tui.NewEventLoading()
 
-	time.Sleep(1 * time.Second)
-	eventsCh <- tui.NewEventSpin("loading tor...")
-	time.Sleep(3 * time.Second)
-	eventsCh <- tui.NewEventAccess("key", "password")
-	time.Sleep(3 * time.Second)
+	// time.Sleep(1 * time.Second)
+	// eventsCh <- tui.NewEventSpin("loading tor...")
+	// time.Sleep(3 * time.Second)
+	// eventsCh <- tui.NewEventAccess("key", "password")
+	// time.Sleep(3 * time.Second)
 	// panic("test")
 
 	<-ctx.Done()
